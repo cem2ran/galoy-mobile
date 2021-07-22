61ccb351510f135b7c3b8f0d6490aa6976209c4c
Cem Turan <cem2ran@gmail.com>
2021-07-22 19:16:48 +0200

    chore: typed i18n keys

--- app/i18n/i18n.ts
+++ app/i18n/i18n.ts
@@ -1,7 +1,21 @@
 import i18n from "i18n-js"
-
 import * as en from "./en.json"
 import * as es from "./es.json"
+
+type ValidPaths<T> = keyof T extends never
+  ? never
+  : {
+      [K in keyof T]: T[K] extends never
+        ? never
+        : T[K] extends Record<string | number | symbol, unknown>
+        ? K extends string
+          ? //@ts-expect-error // wip
+            `${K}.${ValidPaths<T[K]>}`
+          : never
+        : K
+    }[keyof T]
+
+export type keys = ValidPaths<typeof en>
 
 i18n.fallbacks = true
 i18n.translations = { en, es }
--- app/i18n/translate.ts
+++ app/i18n/translate.ts
@@ -1,4 +1,6 @@
 import i18n from "i18n-js"
+import {  keys } from "./i18n"
+
 import type { QuizSectionContent } from "../types/quiz"
 
 /**
@@ -6,8 +8,8 @@ import type { QuizSectionContent } from "../types/quiz"
  *
  * @param key The i18n key.
  */
-export function translate(key: string, options?: Record<string, unknown>): string {
-  return key ? i18n.t(key, options) : null
+export function translate(key: keys, options?: Record<string, unknown>): string {
+  return i18n.t(key, options)
 }
 
 export function translateQuizSections(
